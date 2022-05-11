3c1c0e9912fd7bfe672b186d25c5174496068181
AnupamBasumatary <anupambasumatary16@gmail.com>
2022-05-11 20:25:10 +0530

    User Routes created

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //get log in User -- GET(/api/auth) -- private
 router.get('/', (req, res) => {
