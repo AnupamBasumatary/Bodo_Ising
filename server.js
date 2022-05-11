92712c5f89b13b312a996db28116be29e3a9cd27
AnupamBasumatary <anupambasumatary16@gmail.com>
2022-05-11 19:38:57 +0530

    Connected to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+//Connecting to Database
+connectDB();
 
 //Routes
 app.use('/api/users', require('./routes/users'));
