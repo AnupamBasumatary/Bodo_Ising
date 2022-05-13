7218740f58da014dded3f31fc210ce0dad534f5b
AnupamBasumatary <anupambasumatary16@gmail.com>
2022-05-13 12:10:40 +0530

    Add User info route

--- routes/userDet.js
+++ routes/userDet.js
@@ -18,9 +18,52 @@ router.get('/', auth, async (req, res) => {
 });
 
 //Add User Details -- POST(/api/userdet) -- private
-router.post('/', (req, res) => {
-  res.send('add user details');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('firstName', 'First name is required').not().isEmpty(),
+      check('address1', 'Address Line 1 is required').not().isEmpty(),
+      check('phone', 'phone number is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      firstName,
+      lastName,
+      address1,
+      address2,
+      address3,
+      phone,
+      package,
+    } = req.body;
+
+    try {
+      const newUserDet = new UserDet({
+        firstName,
+        lastName,
+        address1,
+        address2,
+        address3,
+        phone,
+        package,
+        user: req.user.id,
+      });
+
+      const userDetails = await newUserDet.save();
+      res.json(userDetails);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //Update User Details -- PUT(/api/userdet) --private
 router.put('/:id', (req, res) => {
