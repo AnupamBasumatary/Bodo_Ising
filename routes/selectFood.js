44ea1701a981271f315b520eea0212f9d97e6afe
AnupamBasumatary <anupambasumatary16@gmail.com>
2022-05-15 11:19:32 +0530

    PUT request for Users' item with day

--- routes/selectFood.js
+++ routes/selectFood.js
@@ -41,4 +41,40 @@ router.post('/:day', auth, async (req, res) => {
   }
 });
 
+//Update Food for each day --PUT(/api/savefood/:day) --private
+router.put('/:day', auth, async (req, res) => {
+  const { lunch, dinner } = req.body;
+  const day = req.params.day.toString();
+
+  const UpdateDayFoodFields = {};
+  if (lunch) UpdateDayFoodFields.lunch = lunch;
+  if (dinner) UpdateDayFoodFields.dinner = dinner;
+
+  try {
+    let dayFoodDet = await PreFood.findOne({ user: req.user.id });
+    if (!dayFoodDet) {
+      return res.status(404).json({ msg: `Item not selected for ${day}` });
+    }
+
+    console.log(dayFoodDet);
+
+    if (dayFoodDet.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not Authorized' });
+    }
+
+    dayFoodDet = await PreFood.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: UpdateDayFoodFields },
+      { new: true }
+    );
+
+    res.json(dayFoodDet);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//Delete Food --DELETE(/api/savefood/:day) --private
+
 module.exports = router;
