3e41b54d5a67ff0d38808416ed5e017448c9dc29
AnupamBasumatary <anupambasumatary16@gmail.com>
2022-05-15 20:27:13 +0530

    Added DELETED req for Select Food

--- routes/selectFood.js
+++ routes/selectFood.js
@@ -56,8 +56,6 @@ router.put('/:day', auth, async (req, res) => {
       return res.status(404).json({ msg: `Item not selected for ${day}` });
     }
 
-    console.log(dayFoodDet);
-
     if (dayFoodDet.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'Not Authorized' });
     }
@@ -76,5 +74,24 @@ router.put('/:day', auth, async (req, res) => {
 });
 
 //Delete Food --DELETE(/api/savefood/:day) --private
+router.delete('/:day', auth, async (req, res) => {
+  try {
+    let dayFoodDet = await PreFood.findOne({ user: req.user.id });
+    if (!dayFoodDet) {
+      return res.status(404).json({ msg: `Item not selected for ${day}` });
+    }
+
+    if (dayFoodDet.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not Authorized' });
+    }
+
+    dayFoodDet = await PreFood.findOneAndDelete({ user: req.user.id });
+
+    res.json({ msg: 'Contact Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
