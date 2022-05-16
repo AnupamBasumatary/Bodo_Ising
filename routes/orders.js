3c8f48dd51c87708965a5bfc8e3e95948a1ff5b3
AnupamBasumatary <anupambasumatary16@gmail.com>
2022-05-16 12:53:25 +0530

    Order API created -GET + POST

--- routes/orders.js
+++ routes/orders.js
@@ -7,8 +7,14 @@ const Order = require('../models/Order');
 //Get Order Details
 //('/api/order')
 //public
-router.get('/:id', (req, res) => {
-  res.send('Get Order Details');
+router.get('/:id', async (req, res) => {
+  try {
+    const orderDetails = await Order.findById(req.params.id);
+    res.json(orderDetails);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //Add Order Details
@@ -24,12 +30,41 @@ router.post(
     check('price', 'Add Price of Food').not().isEmpty(),
     check('quantity', 'Please add Quantity of Food').not().isEmpty(),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Order Added');
+    const {
+      customerName,
+      address1,
+      address2,
+      phone,
+      foodCode,
+      price,
+      quantity,
+      totalBill,
+    } = req.body;
+
+    try {
+      const newOrder = new Order({
+        customerName,
+        address1,
+        address2,
+        phone,
+        foodCode,
+        price,
+        quantity,
+        totalBill,
+      });
+
+      const order = await newOrder.save();
+      res.json(order);
+      console.log(order._id);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
